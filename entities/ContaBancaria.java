4c61a8ae9c3dba60e5c0ef4699e9d8a765f58c86
Lucas Tukaze <lucastukaze@gmail.com>
2026-02-13 16:55:24 -0300

    Update ContaBancaria.java

--- entities/ContaBancaria.java
+++ entities/ContaBancaria.java
@@ -11,7 +11,7 @@ public class ContaBancaria {
     }
 
     public void exibirDados() {
-        System.out.println("Titular: " + titular);
+        System.out.println("Titular Conta: " + titular);
         System.out.println("Saldo: " + saldo);
     }
 
